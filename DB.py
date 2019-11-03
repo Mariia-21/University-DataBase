27f4716f09cca30b09c1c845f6de777f4b169506
mariia.kusailo <mariia.kusailo@visiquate.com>
2019-11-03 21:28:59 +0200

    Implementation of a function for reading data from a file, displaying on the screen, splitting data into two lists

--- DB.py
+++ DB.py
@@ -1,30 +1,27 @@
 
-
-class Students:
-    def __init__(self, firstName, lastName, DOB, grant, faculty, cource):
+class Person:
+    def __init__(self, firstName, lastName, DOB, grant, faculty, course):
         self.firstName = firstName
         self.lastName = lastName
         self.DOB = DOB
         self.grant = grant
         self.faculty = faculty
-        self.cource = cource
+        self.course = course
 
 
-class Teachers:
-    def __init__(self, firstName, lastName, DOB, grant, faculty, cource):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.DOB = DOB
-        self.grant = grant
-        self.faculty = faculty
-        self.cource = cource
+class Students(Person):
+    pass
+
+
+class Teachers(Person):
+    pass
 
 
 class Controller:
     def __init__(self, path):
         self.path = path
 
-    def readDBFromFile(self):
+    def readDBFromFile(self):   # function for reading all data from file
         users = []
         for line in open(self.path):
             user = []
@@ -32,3 +29,44 @@ class Controller:
                 user.append(i.split('=')[1])
             users.append(user)
         return users
+
+    def printDB(dbName):   # function for printing all data from file
+        firstName = 1
+        lastName = 2
+        birthday = 3
+        money = 4
+        faculty = 5
+        age = 6
+        for user in dbName:
+            print('%-12s %-12s %-14s %-12s %-10s %-5s'
+                  % (user[firstName], user[lastName], user[birthday], user[money], user[faculty], user[age]))
+
+    def createLists(users):  # function for creating two lists (students, teachers)
+        id = 0               # id in file: 1 - students, 2 - teachers
+        firstName = 1
+        lastName = 2
+        birthday = 3
+        money = 4
+        faculty = 5
+        age = 6
+        studentsList = []
+        teachersList = []
+        for user in users:
+            if user[id] == '2':
+                line = Students(user[firstName], user[lastName], user[birthday], user[money], user[faculty], user[age])
+                studentsList.append(line)   # list of all students in database
+            else:
+                line = Teachers(user[firstName], user[lastName], user[birthday], user[money], user[faculty], user[age])
+                teachersList.append(line)   # list of all teachers in database
+        return studentsList, teachersList   # function returned two lists
+
+    def printingList(listName):  # function for printing lists
+        for user in listName:
+            print('%-12s %-12s %-14s %-12s %-10s %-5s'
+                  % (user.firstName, user.lastName, user.DOB, user.grant, user.faculty, user.course))
+
+    def foundYear(year, listName):  # function for search record in database for the entered year
+        for user in listName:
+            if user.DOB[-4:] == year:
+                print('%-12s %-12s %-14s %-12s %-10s %-5s'
+                      % (user.firstName, user.lastName, user.DOB, user.grant, user.faculty, user.course))
--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 import DB
 
-
-print("Выберите следующие действия:")
-print("1 - Вывод всей базы на экран")
-print("2 - Вывод всех студентов базы на экран")
-print("3 - Вывод всех преподавателей базы на экран")
-print("4 - Вывод по заданному году рождения")
-
+i = 1
 users = []
-menu = int(input())
+studentsList = []
+teachersList = []
 
-users = DB.Controller('C:\\Users\\mariia.kusailo\\Project\\input.txt').readDBFromFile()
+while i == 1:
+    print("Выберите следующие действия:\n")
+    print("1 - Вывод всей базы на экран")
+    print("2 - Вывод всех студентов базы на экран")
+    print("3 - Вывод всех преподавателей базы на экран")
+    print("4 - Вывод по заданному году рождения")
+    print("5 - Выход с программы")
+    try:
+        menu = int(input())
+        users = DB.Controller('C:\\Users\\mariia.kusailo\\Project\\input.txt').readDBFromFile()  # reading all data from file
+        studentsList, teachersList = DB.Controller.createLists(users)  # two lists(students, teachers)
 
-print(users)
+        if menu == 1:
+            DB.Controller.printDB(users)  # printing all data from file
 
+        elif menu == 2:
+            print("Список студентов:")
+            DB.Controller.printingList(studentsList)   # printing all students from file
 
+        elif menu == 3:
+            print("Список преподавателей:")
+            DB.Controller.printingList(teachersList)   # printing all teachers from file
 
+        elif menu == 4:
+            year = input("Введите год рождения для поиска ")  # search records in the database for the entered year
+            print("Студенты ", year, " года рождения:\n")
+            DB.Controller.foundYear(year, studentsList)
+            print("Преподаватели ", year, " года рождения:\n")
+            DB.Controller.foundYear(year, teachersList)
+
+        elif menu == 5:
+            quit()             # exit from programme
+
+        else:
+            print("Вы ввели неверное значение!")
+
+    except ValueError:
+        print("Что-то пошло не так :( ")
